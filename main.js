43b37fdd57cff3849c29e1a4db0cfd2c635090ba
javith <akramjavithkhan@gmail.com>
2022-06-16 16:58:52 +0530

    updated js

---
+++ main.js
@@ -0,0 +1,10 @@
+const color="black";
+const click="true";
+
+
+function board(size)
+let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
